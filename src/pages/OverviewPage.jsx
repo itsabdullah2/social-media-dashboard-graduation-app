d3775f16a22d8cf925055cb6676484ce30a77a7d
itsabdullah2 <abdullahosman3020@gmail.com>
2025-04-13 14:39:24 +0200

    initial rendering

---
+++ src/pages/OverviewPage.jsx
@@ -0,0 +1,12 @@
+import Overview from '../components/dashboard-components/overview';
+import Root from '../components/dashboard-components/Root';
+
+const OverviewPage = () => {
+  return (
+    <Root>
+      <Overview />
+    </Root>
+  );
+};
+
+export default OverviewPage;
