892a96da2f9ae8d778f6628f748be5345f1e7c57
itsabdullah2 <abdullahosman3020@gmail.com>
2025-04-18 01:38:01 +0200

    generate Settings page route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import OverviewPage from './pages/OverviewPage';
 import LandingPage from './pages/LandingPage';
+import SettingsPage from './pages/SettingsPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/dashboard" element={<OverviewPage />} />
+      <Route path="/dashboard/settings" element={<SettingsPage />} />
     </Routes>
   );
 }
