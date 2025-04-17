db50eb452fcba93100a55f75172de9d89b1b96a7
itsabdullah2 <abdullahosman3020@gmail.com>
2025-04-17 18:07:01 +0200

    render the components of the dashboard page

--- src/pages/OverviewPage.jsx
+++ src/pages/OverviewPage.jsx
@@ -1,11 +1,19 @@
+import DashboardNavbar from '../components/dashboard-components/DashboardNavbar';
 import Overview from '../components/dashboard-components/overview';
-import Root from '../components/dashboard-components/Root';
+import Sidebar from '../components/dashboard-components/Sidebar';
+import { useAppState } from '../context/AppContext';
 
 const OverviewPage = () => {
+  const { isDarkMode } = useAppState();
+
   return (
-    <Root>
-      <Overview />
-    </Root>
+    <main className={`flex ${isDarkMode ? 'bg-navy' : 'bg-light'}`}>
+      <Sidebar />
+      <div className="flex flex-col w-[1624px] mx-8 gap-5">
+        <DashboardNavbar />
+        <Overview />
+      </div>
+    </main>
   );
 };
 
