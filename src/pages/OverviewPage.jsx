67a2428a3de1f2c4436b05a203eccdfe41239fac
itsabdullah2 <abdullahosman3020@gmail.com>
2025-04-18 01:34:48 +0200

    modify the structure of the component

--- src/pages/OverviewPage.jsx
+++ src/pages/OverviewPage.jsx
@@ -1,19 +1,15 @@
 import DashboardNavbar from '../components/dashboard-components/navbar/DashboardNavbar';
 import Overview from '../components/dashboard-components/overview';
-import Sidebar from '../components/dashboard-components/Sidebar';
-import { useAppState } from '../context/AppContext';
+import Root from '../components/dashboard-components/Root';
 
 const OverviewPage = () => {
-  const { isDarkMode } = useAppState();
-
   return (
-    <main className={`flex ${isDarkMode ? 'bg-navy' : 'bg-light'}`}>
-      <Sidebar />
+    <Root>
       <div className="flex flex-col w-[1624px] mx-8 gap-5">
         <DashboardNavbar />
         <Overview />
       </div>
-    </main>
+    </Root>
   );
 };
 
