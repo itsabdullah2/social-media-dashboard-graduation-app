61c43db3becacd4a0b6edb646a6f790cb0879578
itsabdullah2 <abdullahosman3020@gmail.com>
2025-04-18 01:33:28 +0200

    render settings component

---
+++ src/pages/SettingsPage.jsx
@@ -0,0 +1,12 @@
+import Root from '../components/dashboard-components/Root';
+import Settings from '../components/dashboard-components/settings';
+
+const SettingsPage = () => {
+  return (
+    <Root>
+      <Settings />
+    </Root>
+  );
+};
+
+export default SettingsPage;
