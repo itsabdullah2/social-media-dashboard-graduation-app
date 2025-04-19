9629f668893294daf9e8f086b7553abec04ebe8d
itsabdullah2 <abdullahosman3020@gmail.com>
2025-04-19 19:18:11 +0200

    change the style on state of the sidebar

--- src/pages/OverviewPage.jsx
+++ src/pages/OverviewPage.jsx
@@ -1,11 +1,18 @@
 import DashboardNavbar from '../components/dashboard-components/navbar/DashboardNavbar';
 import Overview from '../components/dashboard-components/overview';
 import Root from '../components/dashboard-components/Root';
+import { useAppState } from '../context/AppContext';
 
 const OverviewPage = () => {
+  const { isSidebarOpen } = useAppState();
+
   return (
     <Root>
-      <div className="flex flex-col w-[1624px] mx-8 gap-5">
+      <div
+        className={`flex flex-col w-[1624px] ${
+          isSidebarOpen ? 'mx-auto px-3 md:px-5 xl:px-8' : 'mx-8'
+        } gap-5`}
+      >
         <DashboardNavbar />
         <Overview />
       </div>
