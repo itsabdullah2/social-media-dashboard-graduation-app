79cabe8eb9e4a8780abc851176763948dd542a84
itsabdullah2 <abdullahosman3020@gmail.com>
2025-04-20 21:53:56 +0200

    add schedule page route

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 import OverviewPage from './pages/OverviewPage';
 import LandingPage from './pages/LandingPage';
 import SettingsPage from './pages/SettingsPage';
+import SchedulePage from './pages/SchedulePage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/dashboard" element={<OverviewPage />} />
+      <Route path="/dashboard/schedule" element={<SchedulePage />} />
       <Route path="/dashboard/settings" element={<SettingsPage />} />
     </Routes>
   );
