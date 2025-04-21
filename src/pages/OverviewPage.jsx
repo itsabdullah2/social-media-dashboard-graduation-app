4234b426b85d4ba25f7ae51b133ada47c4db8453
itsabdullah2 <abdullahosman3020@gmail.com>
2025-04-21 20:39:32 +0200

    remove unused code & add tailwind classes

--- src/pages/OverviewPage.jsx
+++ src/pages/OverviewPage.jsx
@@ -1,15 +1,12 @@
 import DashboardNavbar from '../components/dashboard-components/navbar/DashboardNavbar';
 import Overview from '../components/dashboard-components/overview';
 import Root from '../components/dashboard-components/Root';
-import { useAppState } from '../context/AppContext';
 
 const OverviewPage = () => {
-  const { isSidebarOpen } = useAppState();
-
   return (
     <Root>
       <div
-        className={`flex flex-col w-full max-w-[1624px] mx-auto px-3 md:px-5 xl:px-8 gap-5`}
+        className={`flex-1 flex flex-col max-w-[1624px] mx-auto px-3 md:px-5 xl:px-8 gap-5`}
       >
         <DashboardNavbar />
         <Overview />
