a0bd7879b97b49a2a4052848ba839aa7a77fc5ae
itsabdullah2 <abdullahosman3020@gmail.com>
2025-04-21 14:19:17 +0200

    generate the routes of signin, signup pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,15 @@ import OverviewPage from './pages/OverviewPage';
 import LandingPage from './pages/LandingPage';
 import SettingsPage from './pages/SettingsPage';
 import SchedulePage from './pages/SchedulePage';
+import SignInPage from './pages/SignInPage';
+import SignUpPage from './pages/SignUpPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
+      <Route path="/signin" element={<SignInPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
       <Route path="/dashboard" element={<OverviewPage />} />
       <Route path="/dashboard/schedule" element={<SchedulePage />} />
       <Route path="/dashboard/settings" element={<SettingsPage />} />
