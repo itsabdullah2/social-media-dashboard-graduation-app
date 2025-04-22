8a2aefd2607fb3608026b8b792470fe5c7e281fd
itsabdullah2 <abdullahosman3020@gmail.com>
2025-04-22 20:19:00 +0200

    generate the UI of the post

--- src/components/dashboard-components/schedule/Post.jsx
+++ src/components/dashboard-components/schedule/Post.jsx
@@ -1,19 +1,51 @@
+import { useAppState } from '../../../context/AppContext';
+import { Link } from 'react-router-dom';
+import { MdEdit, MdDelete } from 'react-icons/md';
+
 const Post = ({ post }) => {
+  const { isDarkMode, postImage } = useAppState();
+
   return (
-    <div className="flex flex-col justify-center gap-2 bg-indigo-500 rounded-lg shadow p-2">
-      <div className="flex gap-2">
-        <img
-          alt="post-image"
-          className="bg-light w-24 h-24 shrink-0 rounded-lg"
-        />
-        <div className="flex flex-col text-white">
-          <span className="font-bold italic">{post.postTitle}</span>
-          <p className="line-clamp-3">{post.postDescription}</p>
+    <div
+      className={`flex flex-col justify-center gap-5 shadow-lg ${
+        isDarkMode ? 'bg-navy' : 'bg-blueberry'
+      } rounded-lg shadow p-3`}
+    >
+      <img
+        src={postImage}
+        alt="post image"
+        className="w-full max-h-32 rounded-md bg-cover bg-center"
+      />
+      <div className={`flex flex-col text-white`}>
+        <span className={`font-bold italic`}>{post.title}</span>
+        <p className={``}>{post.description}</p>
+      </div>
+      <div className="flex items-center justify-between">
+        <Link
+          to={`/dashboard/schedule/${post.id}`}
+          className={`text-center font-bold text-blueberry bg-light ${
+            isDarkMode ? 'hover:text-white hover:bg-blueberry' : ''
+          } duration-200 rounded-md p-2 cursor-pointer`}
+        >
+          See more
+        </Link>
+        <div className="flex items-center gap-2">
+          <button
+            className={`rounded-md py-2 px-4 cursor-pointer border ${
+              isDarkMode
+                ? 'border-blueberry text-blueberry'
+                : 'border-white text-white'
+            }`}
+          >
+            <MdEdit size={20} />
+          </button>
+          <button
+            className={`rounded-md py-2 px-4 cursor-pointer border border-darkBluishGray text-darkBluishGray`}
+          >
+            <MdDelete size={20} />
+          </button>
         </div>
       </div>
-      <button className="hover:bg-purple-300 bg-neutral-50 font-bold text-indigo-500 rounded p-2">
-        See more
-      </button>
     </div>
   );
 };
