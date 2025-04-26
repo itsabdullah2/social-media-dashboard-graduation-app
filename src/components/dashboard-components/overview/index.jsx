e809ab9eb3b28cc82d12767d1a6505755bd528f7
itsabdullah2 <abdullahosman3020@gmail.com>
2025-04-26 23:51:00 +0300

    change pathes

--- src/components/dashboard-components/overview/index.jsx
+++ src/components/dashboard-components/overview/index.jsx
@@ -1,11 +1,11 @@
-import Audience from '../charts/Audience/Audience';
-import StatesComponent from '../charts/statesBox/StatesComponent';
-import ActivityComponent from '../charts/ActivityChart/ActivityComponent';
-import RevenueComponent from '../charts/RevenueChart/RevenueComponent';
-import OverviewComponent from '../charts/overviewChart/OverviewComponent';
-import AgeComponent from '../charts/AgeChart/AgeComponent';
-import GenderComponent from '../charts/GenderCharts/GenderComponent';
-import ShedulePosts from '../charts/SchedulePosts/ShedulePosts';
+import Audience from '../../charts/Audience/Audience';
+import StatesComponent from '../../charts/statesBox/StatesComponent';
+import ActivityComponent from '../../charts/ActivityChart/ActivityComponent';
+import RevenueComponent from '../../charts/RevenueChart/RevenueComponent';
+import OverviewComponent from '../../charts/overviewChart/OverviewComponent';
+import AgeComponent from '../../charts/AgeChart/AgeComponent';
+import GenderComponent from '../../charts/GenderCharts/GenderComponent';
+import ShedulePosts from '../../charts/SchedulePosts/ShedulePosts';
 
 const Overview = () => {
   return (
