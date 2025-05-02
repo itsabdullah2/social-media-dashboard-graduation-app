54f87dd965ea90a20d6e04a28eca05aacf391f4f
itsabdullah2 <abdullahosman3020@gmail.com>
2025-05-02 20:29:47 +0300

    render an illustration of there is no post created

--- src/components/dashboard-components/schedule/PostsList.jsx
+++ src/components/dashboard-components/schedule/PostsList.jsx
@@ -1,15 +1,29 @@
-import { useEffect } from 'react';
-import { useAppState } from '../../../context/AppContext';
-import Post from './Post';
+import { useAppState } from "../../../context/AppContext";
+import Post from "./Post";
+import socialMedia from "../../../assets/social-media.png";
 
 const PostsList = () => {
-  const { posts } = useAppState();
+  const { posts, isDarkMode } = useAppState();
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-      {posts.map((post, i) => (
-        <Post key={i} post={post} />
-      ))}
+    <div className="relative w-full h-full">
+      {posts?.length > 0 ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+          {posts?.map((post, i) => (
+            <Post key={i} post={post} />
+          ))}
+        </div>
+      ) : (
+        <div
+          className={`absolute left-1/2 top-20 -translate-x-1/2 opacity-85 w-96`}
+        >
+          <img
+            src={socialMedia}
+            alt="social media illustration image"
+            className="w-full max-w-full"
+          />
+        </div>
+      )}
     </div>
   );
 };
