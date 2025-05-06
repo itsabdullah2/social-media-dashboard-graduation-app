d75fea7506d49afc3ff005873be50a7ef21c4508
anas <anasahmed7c7@gmail.com>
2025-05-06 20:52:58 +0300

    implementing landing page with charts and icons and applying Dark mode to it

--- src/components/landing/index.jsx
+++ src/components/landing/index.jsx
@@ -1,10 +1,13 @@
 import Header from './Header';
 import About from './About';
 import TryItNow from './TryItNow';
+import { useAppState } from '../../context/AppContext';
 
 const Landing = () => {
+  const { isDarkMode } = useAppState();
+  
   return (
-    <main className="max-w-[1624px] mx-auto">
+    <main className={`max-w-[1624px] mx-auto ${isDarkMode ? 'bg-navy' : 'bg-white'}`}>
       <Header />
       <About />
       <TryItNow />
