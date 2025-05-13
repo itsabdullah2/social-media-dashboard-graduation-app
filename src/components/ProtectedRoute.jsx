67290b650d9ffb2f5e5fa6b71afa3cb2e421d782
itsabdullah2 <abdullahosman3020@gmail.com>
2025-05-13 04:43:46 +0300

    remove unused import

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,7 +1,6 @@
-import { useSession } from '@supabase/auth-helpers-react';
-import { Navigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthProvider';
-import LoadingSpinner from './LoadingSpinner';
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthProvider";
+import LoadingSpinner from "./LoadingSpinner";
 
 const ProtectedRoute = ({ children }) => {
   const { user, loading } = useAuth();
