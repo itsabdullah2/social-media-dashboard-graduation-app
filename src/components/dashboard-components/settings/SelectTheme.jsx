7b28e5352d9a5a5d129244d560651921f8062c22
itsabdullah2 <abdullahosman3020@gmail.com>
2025-05-14 08:44:28 +0300

    work on theme fucntions

--- src/components/dashboard-components/settings/SelectTheme.jsx
+++ src/components/dashboard-components/settings/SelectTheme.jsx
@@ -1,10 +1,14 @@
-import { useAppState } from '../../../context/AppContext';
+import { useState } from "react";
+import { useAppState } from "../../../context/AppContext";
 
 const commonStyles =
-  'py-2 px-6 rounded-md bg-light cursor-pointer text-navy font-medium duration-200';
+  "py-2 px-6 rounded-md bg-light cursor-pointer text-navy font-medium duration-200 border border-transparent";
 
 const SelectTheme = () => {
   const { setIsDarkMode, handleSystemTheme, isDarkMode } = useAppState();
+  const [selectedTheme, setSelectedTheme] = useState(
+    isDarkMode ? "Dark" : "Light"
+  );
 
   const handleLightTheme = () => {
     setIsDarkMode(false);
@@ -12,20 +16,47 @@ const SelectTheme = () => {
   const handleDarkTheme = () => {
     setIsDarkMode(true);
   };
+  const handleSelectedTheme = (theme) => {
+    setSelectedTheme(theme);
+  };
 
   return (
     <div className="flex flex-col gap-2">
-      <p className={`${isDarkMode ? 'text-white' : 'text-navy'} font-medium`}>
+      <p className={`${isDarkMode ? "text-white" : "text-navy"} font-medium`}>
         Select Theme
       </p>
       <div className="flex items-center gap-2">
-        <button className={`${commonStyles}`} onClick={handleLightTheme}>
+        <button
+          className={`${commonStyles} ${
+            selectedTheme === "Light" ? "!border-blueberry" : ""
+          }`}
+          onClick={() => {
+            handleLightTheme();
+            handleSelectedTheme("Light");
+          }}
+        >
           Light Mode
         </button>
-        <button className={`${commonStyles}`} onClick={handleDarkTheme}>
+        <button
+          className={`${commonStyles} ${
+            selectedTheme === "Dark" ? "!border-blueberry" : ""
+          }`}
+          onClick={() => {
+            handleDarkTheme();
+            handleSelectedTheme("Dark");
+          }}
+        >
           Dark Mode
         </button>
-        <button className={`${commonStyles}`} onClick={handleSystemTheme}>
+        <button
+          className={`${commonStyles} ${
+            selectedTheme === "System" ? "!border-blueberry" : ""
+          }`}
+          onClick={() => {
+            handleSystemTheme();
+            handleSelectedTheme("System");
+          }}
+        >
           System
         </button>
       </div>
