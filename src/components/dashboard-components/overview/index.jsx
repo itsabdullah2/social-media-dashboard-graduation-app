daca29465d799c36df730028eb13aea4dd474781
itsabdullah2 <abdullahosman3020@gmail.com>
2025-05-15 16:03:31 +0300

    solve resposiveness issues

--- src/components/dashboard-components/overview/index.jsx
+++ src/components/dashboard-components/overview/index.jsx
@@ -1,18 +1,18 @@
-import Audience from "../../charts/Audience/Audience";
-import StatesComponent from "../../charts/statesBox/StatesComponent";
-import ActivityComponent from "../../charts/ActivityChart/ActivityComponent";
-import RevenueComponent from "../../charts/RevenueChart/RevenueComponent";
-import OverviewComponent from "../../charts/OverviewChart/OverviewComponent";
-import AgeComponent from "../../charts/AgeChart/AgeComponent";
-import GenderComponent from "../../charts/GenderCharts/GenderComponent";
-import SchedulePosts from "../../charts/SchedulePosts/SchedulePosts";
+import Audience from '../../charts/Audience/Audience';
+import StatesComponent from '../../charts/statesBox/StatesComponent';
+import ActivityComponent from '../../charts/ActivityChart/ActivityComponent';
+import RevenueComponent from '../../charts/RevenueChart/RevenueComponent';
+import OverviewComponent from '../../charts/OverviewChart/OverviewComponent';
+import AgeComponent from '../../charts/AgeChart/AgeComponent';
+import GenderComponent from '../../charts/GenderCharts/GenderComponent';
+import SchedulePosts from '../../charts/SchedulePosts/SchedulePosts';
 
 const Overview = () => {
   return (
     <div className="flex flex-col gap-4 pb-4 px-2 sm:px-4">
       <StatesComponent />
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
         <div className="w-full">
           <OverviewComponent />
         </div>
@@ -24,7 +24,7 @@ const Overview = () => {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 xl::grid-cols-4 gap-4">
         <div className="w-full">
           <GenderComponent />
         </div>
